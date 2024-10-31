ab7bf277b41bfff367d8ffa783127c41ed959da3
Kirita Riku <rikuribo1128@icloud.com>
2024-10-31 15:34:08 +0900

    plus test

--- test.bash
+++ test.bash
@@ -3,7 +3,7 @@
 #SPDX-License-Identifier: BSD-3-Clause
 
 ng (){
-	echo ${1}行目が違います
+	echo "${1}行目が違います"
 	res=1
 }
 
@@ -15,11 +15,11 @@ out=$(seq 5 | ./plus)
 
 out=$(echo あ | ./plus)
 [ "$?" = 1 ]      || ng "$LINENO"
-[ "${out}" = ""] || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
 
 out=$(echo | ./plus)
-[ "$?" = 1]       || ng "$LINENO"
-[ "${out}" = ""] || ng "$LINENO"
+[ "$?" = 1 ]       || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
 
-[ "$res" = 0] && echo OK
+[ "$res" = 0 ] && echo OK
 exit $res
