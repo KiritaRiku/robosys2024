ff9b839531bdce1812a2053784ca38b48b5e0b47
Kirita Riku <rikuribo1128@icloud.com>
2024-10-31 15:26:00 +0900

    plus test

--- test.bash
+++ test.bash
@@ -9,18 +9,17 @@ ng (){
 
 res=0
 
-###通常の入力###
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng "$LINENO"
 
-###間違った入力###
+
 out=$(echo あ | ./plus)
 [ "$?" = 1 ]      || ng "$LINENO"
-[ "$?{out}" = ""] || ng "$LINENO"
+[ "${out}" = ""] || ng "$LINENO"
 
 out=$(echo | ./plus)
 [ "$?" = 1]       || ng "$LINENO"
-[ "$?{out}" = ""] || ng "$LINENO"
+[ "${out}" = ""] || ng "$LINENO"
 
 [ "$res" = 0] && echo OK
 exit $res
