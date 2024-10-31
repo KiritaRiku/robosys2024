14956e6fb0886ba95f65b2854255cc4b3805b269
Kirita Riku <rikuribo1128@icloud.com>
2024-10-31 12:20:17 +0900

    regression test

---
+++ test.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SPDX-FileCopyrightText: 2024 Kirita Riku <rikuribo1128@icloud.com>
+#SPDX-License-Identifier: BSD-3-Clause
+
+ng(){
+	echo ${1}行目が違う
+	res=1
+}
+
+res=0
+
+out=$(seq 5 | .plus)
+[ "{out}" = 15] || ng "$LINENO"
+
+[ "{res}" = 0] && echo OK
+exit $res
