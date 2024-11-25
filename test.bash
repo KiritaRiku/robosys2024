77a66289553ec905b4d993e3ab6776355155a957
Kirita Riku <rikuribo1128@icloud.com>
2024-11-26 00:06:59 +0900

    factorial test

--- test.bash
+++ test.bash
@@ -10,7 +10,11 @@ ng (){
 res=0
 
 out=$(seq 5 | ./factorial)
-[ "${out}" = "5の階乗は120です。" ] || ng "$LINENO"
+[ "${out}" = "1の階乗は1です。
+2の階乗は2です。
+3の階乗は6です。
+4の階乗は24です。
+5の階乗は120です。" ] || ng "$LINENO"
 
 [ "$res" = 0 ] && echo OK
 exit $res
