3e01dbc90275d1761a9dbf8ff1113979ad158f6f
Kirita Riku <rikuribo1128@icloud.com>
2024-11-26 01:10:09 +0900

    assiment1 test

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash -xv
+#!/bin/bash 
 #SPDX-FileCopyrightText: 2024 Kirita Riku <rikuribo1128@icloud.com>
 #SPDX-License-Identifier: BSD-3-Clause
 
@@ -10,11 +10,28 @@ ng (){
 res=0
 
 out=$(seq 5 | ./factorial)
-[ "${out}" = "1の階乗は1です。
-2の階乗は2です。
-3の階乗は6です。
-4の階乗は24です。
-5の階乗は120です。" ] || ng "$LINENO"
+ans="1! = 1
+2! = 2
+3! = 6
+4! = 24
+5! = 120"
+
+#1,2,3,4,5の数字の階乗の計算正しいか
+[ "${out}" = "$ans" ] || ng "$LINENO"
+
+#負の数を入力した時
+out=$(seq -2 | ./factorial)
+[ "$out" = "負ではない整数を入力してください"] && ng "$LINENO"
+
+#文字を入力したとき
+out=$(echo あ | ./factorial)
+[ "$out" = "エラーが発生しました。" ] || ng "$LINENO"
+
+#空白
+out=$(echo "" | ./factorial)
+[ "$out" = "エラーが発生しました。" ] || ng "$LINENO"
+
+
 
 [ "$res" = 0 ] && echo OK
 exit $res
